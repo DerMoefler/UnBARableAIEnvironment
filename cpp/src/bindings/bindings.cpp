c1626b4aa3ec706e9f261b444d753dfccef591e1
d.fastenau <d.fastenau@stud.phwt.de>
2026-06-02 09:26:04 +0000

    pybind first layout

---
+++ cpp/src/bindings/bindings.cpp
@@ -0,0 +1,55 @@
+#include <pybind11/pybind11.h>
+
+#include "../unit/unit.h"
+#include "../unit/pawn.h"
+#include "../unit_data/unit_data.h"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(bar_ai, m) {
+    m.doc() = "Python bindings for UnBARableAI";
+
+    using UnBARableAI::unit::Unit;
+    using UnBARableAI::unit::Pawn;
+    using UnBARableAI::unit::UnitData;
+
+    // -------------------------
+    // UnitData
+    // -------------------------
+    // UnitData ist ein einfacher Struct-Typ mit öffentlichen Feldern.
+    // Solche Typen bindet man direkt mit def_readwrite. [1](https://pybind11.readthedocs.io/en/stable/classes.html)
+    py::class_<UnitData>(m, "UnitData")
+        .def(py::init<>())
+        .def_readwrite("health", &UnitData::health)
+        .def_readwrite("team", &UnitData::team)
+        .def_readwrite("xPosition", &UnitData::xPosition)
+        .def_readwrite("yPosition", &UnitData::yPosition)
+        .def_readwrite("zPosition", &UnitData::zPosition)
+        .def_readwrite("hasCurrentCommand", &UnitData::hasCurrentCommand);
+
+    // -------------------------
+    // Unit (abstrakte Basisklasse)
+    // -------------------------
+    // Keine .def(py::init<>()) !
+    // Unit ist abstrakt und nicht direkt konstruierbar. [2](https://pybind11.readthedocs.io/en/stable/advanced/classes.html)
+    py::class_<Unit>(m, "Unit")
+        .def("getHealth", &Unit::getHealth)
+        .def("getTeam", &Unit::getTeam)
+        .def("getXPosition", &Unit::getXPosition)
+        .def("getYPosition", &Unit::getYPosition)
+        .def("getZPosition", &Unit::getZPosition)
+        .def("hasCurrentCommand", &Unit::hasCurrentCommand);
+
+    // -------------------------
+    // Pawn : Unit
+    // -------------------------
+    // Vererbung wird mit py::class_<Pawn, Unit> angegeben. [1](https://pybind11.readthedocs.io/en/stable/classes.html)[2](https://pybind11.readthedocs.io/en/stable/advanced/classes.html)
+    py::class_<Pawn, Unit>(m, "Pawn")
+        .def(py::init<UnitData>(), py::arg("data"))
+        .def("getHealth", &Pawn::getHealth)
+        .def("getTeam", &Pawn::getTeam)
+        .def("getXPosition", &Pawn::getXPosition)
+        .def("getYPosition", &Pawn::getYPosition)
+        .def("getZPosition", &Pawn::getZPosition)
+        .def("hasCurrentCommand", &Pawn::hasCurrentCommand);
+}
