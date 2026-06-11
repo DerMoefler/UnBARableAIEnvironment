3c52d719dfdf60c6c83559a4fd0ad2d4dab4cc4a
Lukas <lu.meyer@stud.phwt.de>
2026-06-11 20:49:52 +0000

    changed bar_env with pawndata

--- python/src/environment/bar_environment.py
+++ python/src/environment/bar_environment.py
@@ -53,13 +53,6 @@ class BAR_Environment:
         data = bar_ai.UnitData()
         pawn = bar_ai.Pawn(data)
 
-        enemypawn0 = bar_ai.Pawn(data)
-        enemypawn1 = bar_ai.Pawn(data)
-        enemypawn2 = bar_ai.Pawn(data)
-
-        allypawn0 = bar_ai.Pawn(data)
-        allypawn1 = bar_ai.Pawn(data)
-
         # placeholder for all information
         enemy_max, enemy_feat = self.get_enemy_feat_size()  # (max_enemies, features_per_enemy)
         ally_max, ally_feat = self.get_ally_feat_size()    # (max_allies, features_per_ally)
@@ -68,7 +61,6 @@ class BAR_Environment:
         own_feats = np.zeros(own_feat_size, dtype=np.float32)
         enemy_features = np.zeros((enemy_max, enemy_feat), dtype=np.float32)
         ally_features = np.zeros((ally_max, ally_feat), dtype=np.float32)
-        agent_id_feats = np.zeros(self.get_n_agents(), dtype=np.float32)
 
         unit_type = pawn.getUnitType()
         # available_actions = self.get_available_actions(unit_type).flatten()
@@ -78,21 +70,17 @@ class BAR_Environment:
             pos_x = pawn.getXPosition()
             pos_y = pawn.getYPosition()
             pos_z = pawn.getZPosition()
-            health_percentage = self.get_health_percentage(health, self.get_health_max(agent_id))
-            sight_radius = self.unit_sight_radius(unit_type)
+            health_percentage = self.get_health_percentage(health, pawn.getMaxHealth())
+            sight_radius = pawn.getSightRange()
 
 
             own_feats[:7] = np.array([float(unit_type), float(pos_x), float(pos_y), float(pos_z), float(health), float(health_percentage), float(sight_radius)], dtype=np.float32)
 
-            enemy_idx = 0
-            for enemy_unit in pawn.getUnitsInSight():
+
+            for enemy_unit in pawn.getUnitsInSight(): #pawn.getEnemyUnitsInSight(): should get an array of unit objects
                 if enemy_unit.getTeam() != pawn.getTeam():
-                    try:
-                        enemy_unit_id = int(enemy_unit)
-                    except Exception:
-                        continue
                     enemy_unit_type = enemy_unit.getUnitType()
-                    enemy_pos = np.asarray(self.get_relative_pos(pawn.getUnitID(), enemy_unit)).flatten()
+                    enemy_pos = np.asarray(self.get_relative_pos(pawn, enemy_unit)).flatten()
                     enemy_health = enemy_unit.getHealth()
 
                     enemy_features[enemy_idx, :3] = [
@@ -100,25 +88,19 @@ class BAR_Environment:
                         float(enemy_pos[0]) if enemy_pos.size > 0 else 0.0,
                         float(enemy_health),
                     ]
-                    enemy_idx += 1
 
-            ally_idx = 0
+
             for ally_unit in pawn.getUnitsInSight():
                 if ally_unit.getTeam() == pawn.getTeam():
-                    try:
-                        ally_unit_id = int(ally_unit)
-                    except Exception:
-                        continue
                     ally_unit_type = ally_unit.getUnitType()
-                ally_pos = np.asarray(self.get_relative_pos(pawn.getUnitID(), ally_unit)).flatten()
-                ally_health = ally_unit.getHealth()
+                    ally_pos = np.asarray(self.get_relative_pos(pawn, ally_unit)).flatten()
+                    ally_health = ally_unit.getHealth()
 
-                ally_features[ally_idx, :3] = [
-                    float(ally_unit_type),
-                    float(ally_pos[0]) if ally_pos.size > 0 else 0.0,
-                    float(ally_health),
-                ]
-                ally_idx += 1
+                    ally_features[ally_idx, :3] = [
+                        float(ally_unit_type),
+                        float(ally_pos[0]) if ally_pos.size > 0 else 0.0,
+                        float(ally_health),
+                    ]
 
 
         local_obs = np.concatenate(
@@ -126,51 +108,32 @@ class BAR_Environment:
                 own_feats.flatten(),
                 enemy_features.flatten(),
                 ally_features.flatten(),
-                agent_id_feats.flatten(),
             )
         )
 
         return local_obs
 
     def get_enemy_feat_size(self):
-        # return (max_enemies, features_per_enemy)
+        # return (max_enemies, features_per_enemy), hardcoded for now
         return (5, 3)
     
     def get_ally_feat_size(self):
-        # return (max_allies, features_per_ally)
+        # return (max_allies, features_per_ally), hardcodede for now
         return (5, 2)
     
     def get_own_feat_size(self):
+        #might be changed in the future, limited to 7 right now, because of unit_type, posx, posy, posz, health, health_percentage, sight_radius
         return 7
-    
-    def get_unit_by_id(self, unit_id):
-        return 0
-    
-    def get_health_max(self, unit_id):
-        return 100.0
     
     def get_health_percentage(self, health, health_max):
         return health / health_max
     
-    def unit_sight_radius(self, unit_type):
-        if isinstance(unit_type, int):
-            if unit_type == 0:
-                u = "pawn"
-            elif unit_type == 1:
-                u = "commander"
-
-        if u == "pawn":
-            return 429.0
-        if u == "commander":
-            return 450.0
-        return 0
-    
-    def get_relative_pos(self, unit_id, second_unit_id):
-        #posx1 - posx2
-        #posy1 - posy2
-        #posz1 - posz2
-        #rel_pos = np.array(relx, rely, relz)
-        return 0 #rel_pos
+    def get_relative_pos(self, agent, second_unit_id):
+        relx = second_unit_id.getXPosition() - agent.getXPosition()
+        rely = second_unit_id.getYPosition() - agent.getYPosition()
+        relz = second_unit_id.getZPosition() - agent.getZPosition()
+        rel_pos = np.array([relx, rely, relz], dtype=np.float32)
+        return rel_pos
 
     # def get_available_actions(self, unit_type):
     #     #[move_up, move_down, move_left, move_right, attack, stay, build]
