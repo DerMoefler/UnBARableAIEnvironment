7a382037d3c3cbcf050e75675adfab71ee16b611
d.fastenau <d.fastenau@stud.phwt.de>
2026-06-26 07:48:34 +0000

    switched back to old client versions bc i dont like impl + changed the cmake so building works with these changes + changed the proto service name bc it was the same as our EngineAI

--- cpp/include/UnBARableAI/UnBARableAIClient.h
+++ cpp/include/UnBARableAI/UnBARableAIClient.h
@@ -1,9 +1,12 @@
 #pragma once
 
+#include <grpcpp/grpcpp.h>
+#include <google/protobuf/empty.pb.h>
 
 #include <memory>
 #include <string>
 
+#include "UnBARableAI.grpc.pb.h"
 
 class UnBARableAIClient {
 public:
@@ -14,13 +17,6 @@ public:
      */
     explicit UnBARableAIClient(const std::string& target = "127.0.0.1:50051");
 
-    ~UnBARableAIClient();
-
-    UnBARableAIClient(UnBARableAIClient&&) noexcept;    
-    UnBARableAIClient& operator=(UnBARableAIClient&&) noexcept;    
-
-    UnBARableAIClient(const UnBARableAIClient&) = delete;    
-    UnBARableAIClient& operator=(const UnBARableAIClient&) = delete;
     /**
      * @brief Sendet den RPC `handleEventUpdate` an den Python-Server.
      *
@@ -34,16 +30,19 @@ public:
      *
      * @return Letzte Fehlermeldung oder leerer String.
      */
-    const std::string& GetLastError() const;
+    const std::string& GetLastError() const { return last_error_; }
 
     /**
      * @brief Liefert das aktuell konfigurierte Ziel.
      *
      * @return Zieladresse des Servers.
      */
-    const std::string& GetTarget() const;
+    const std::string& GetTarget() const { return target_; }
 
 private:
-    class Impl;
-    std::unique_ptr<Impl> impl_;
+    std::string target_;
+    std::string last_error_;
+
+    std::shared_ptr<grpc::Channel> channel_;
+    std::unique_ptr<UnBARableAIService::Stub> stub_;
 };
