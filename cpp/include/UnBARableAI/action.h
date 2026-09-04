1de5750491581740d23d6dfe4aaab26e4fe1f68e
Dorian Fastenau <d.fastenau@stud.phwt.de>
2026-09-04 12:32:40 +0000

    RE and Enum for ActionID

--- cpp/include/UnBARableAI/action.h
+++ cpp/include/UnBARableAI/action.h
@@ -2,12 +2,20 @@
 
 namespace UnBARableAINS {
 
+enum class ActionId : int {
+    MoveRight = 1,
+    MoveLeft  = 2,
+    MoveUp    = 3,
+    MoveDown  = 4,
+    Attack    = 5
+};
+
 struct Action {
     int unit_id;
     int team_id;
     int ally_team_id;
-    int action_id; // 1: move right, 2: move left, 3: move up, 4: move down, 5: attack
-    int target_unit_id; // Only used for attack action
+    ActionId action_id;
+    int target_unit_id; // Only used for Attack
 };
 
-}; // namespace UnBARableAI
+} // namespace UnBARableAINS
